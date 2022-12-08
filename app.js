d85fe8b126a691e633436edd20a4c7f988706626
kokspro <koks.programming@gmail.com>
2022-12-08 14:03:09 -0600

    Structure updated
    App pictures resized to be uniform
    Button links to apps were linked
    Card Titles updated
    Font Family added

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         entry.target.classList.toggle('show', entry.isIntersecting);
     })
+    },
+    {
+        threshold: 0.25
     }
 );
 
